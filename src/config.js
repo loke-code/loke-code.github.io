6e1cf52dfca25d76591e9159db112277335abbc9
Lokesh Thangamani <lokesht1507@gmail.com>
2025-08-21 23:35:01 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,5 @@
+function getConfig() {
+    return {
+        Instance: "Aq3ZljPXt8lYC11ddY4TtR85bWVRdd6eU74nzs9oJszKcQXf0h9DJQQJ99BHACHYHv6AArohAAABAZBSslVf.4FKfsqiLWGJKc6utzNuCdnqEl3sTW8yCjpI4dZgf9dM8wpho9pwSJQQJ99BHACHYHv6AArohAAABAZBS3VHY"
+    };
+}
